21cbaa11957ab86903891fe9775da4f7e68d19ca
Chase Pietrangelo <dev@chasepie.com>
2026-04-19 14:30:44 -0400

    Simplify HTML/CSS & Fix PDF Rendering (#3)
    * revamp
    
    * dark mode
    
    * tweaks
    
    * fix list and fonts
    
    * refactor layout and styles: replace <hr> with a styled divider and clean up print styles
    
    * review

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -4,7 +4,6 @@ import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-header',
-  imports: [],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -15,7 +14,7 @@ export class Header implements AfterViewInit {
   protected readonly location = environment.location;
   protected readonly phone = environment.phone;
   protected readonly email = environment.email;
-  protected readonly linkedin = environment.linkedin;
+  protected readonly github = environment.github;
 
   public ngAfterViewInit(): void {
     replace();
